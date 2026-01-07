9e4e35c8f2af578c5b798b595732581d00dfb871
Linus <younglin90@tcapsstudent.net>
2026-01-06 19:32:16 -0500

    Refactor level1 walls and add descriptive text

--- level1.js
+++ level1.js
@@ -1,16 +1,14 @@
-// Level 1 definition
 window.level1 = {
     startX: 50,
     startY: 50,
     walls: [
-        // Outer walls
-        {x: 0, y: 0, width: 640, height: 20},       // top wall
-        {x: 0, y: 0, width: 20, height: 480},       // left wall
-        {x: 620, y: 0, width: 20, height: 480},     // right wall
-        // Inner obstacles
-        {x: 100, y: 100, width: 150, height: 20},   // horizontal top obstacle
-        {x: 250, y: 200, width: 20, height: 150},   // vertical middle obstacle
-        {x: 150, y: 350, width: 300, height: 20}    // horizontal bottom obstacle
+        {x: 0, y: 0, width: 640, height: 20},
+        {x: 0, y: 0, width: 20, height: 480},
+        {x: 620, y: 0, width: 20, height: 480},
+        {x: 100, y: 100, width: 150, height: 20},
+        {x: 250, y: 200, width: 20, height: 150},
+        {x: 150, y: 350, width: 300, height: 20}
     ],
-    nextLevel: null // will be automatically linked in index.html
+    text: "*you are in the Dark, Dank city at 3:00AM... You could not sleep.",
+    nextLevel: null
 };
